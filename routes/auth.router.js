8df0928b6e785fe2ae611f34ca966c10b99b73fd
IvanZaborskih <ivan.31.03@mail.ru>
2022-08-18 23:49:24 +0300

    Some fixes

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -8,7 +8,6 @@ router.post('/registration', [
 	check('password', 'Password must have more 4 symbols').isLength({ min: 4 })
 ], authController.registration);
 router.post('/login', authController.login);
-router.get('/auth', authMiddleware, authController.check);
 router.get('/users', authMiddleware, authController.getAllUsers);
 
 module.exports = router;
